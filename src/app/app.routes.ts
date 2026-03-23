81680e20063d95aa30786db36df7b0fae0db0634
Mustafa Sarigul <m.sarigul199@gmail.com>
2026-03-23 21:17:41 +0300

    training module created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,13 +17,10 @@ export const routes: Routes = [
   {
     path: 'signin',
     loadComponent: () => import('./modules/auth/pages/signin/signin.page').then( m => m.SigninPage)
-  },  {
-    path: 'training-list',
-    loadComponent: () => import('./modules/training/pages/training-list/training-list.page').then( m => m.TrainingListPage)
   },
   {
-    path: 'training-details',
-    loadComponent: () => import('./modules/training/pages/training-details/training-details.page').then( m => m.TrainingDetailsPage)
+    path: 'training-list',
+    loadComponent: () => import('./modules/training/pages/training-list/training-list.page').then( m => m.TrainingListPage)
   },
   {
     path: 'training-detail',
